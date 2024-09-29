f3767457620d60e8a69e0ae7c02ea68481c8c98b
Anna <32278291+annasudol@users.noreply.github.com>
2024-09-29 20:13:39 +0200

    wasm memory

--- www/index.js
+++ www/index.js
@@ -1,12 +1,22 @@
     
     
     async function init() {
+      const importantObj = {
+        console: {
+          log: () => {
+            console.log("just logging")
+          },
+          error: () => {
+            console.log("error")
+          }
+        }
+      }
       const response = await fetch("sum.wasm");
       const buffer = response.arrayBuffer();
-        const wasm = await WebAssembly.instantiate(buffer);
+      const wasm = await WebAssembly.instantiate(buffer, importantObj);
 
-        const sumFunction = wasm.instance.exports.sum;
-        const result = sumFunction(100, 1000);
-        console.log(result);
-      }
-    init();
+      const sumFunction = wasm.instance.exports.sum;
+      const result = sumFunction(100, 1000);
+      console.log(result);
+  }
+  init();
