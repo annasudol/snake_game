920e298c32c53a4c8dcc529083fd20ef98c2382e
Anna <32278291+annasudol@users.noreply.github.com>
2024-09-30 12:48:25 +0200

    chnaged canvas settings

--- www/index.js
+++ www/index.js
@@ -3,5 +3,8 @@ import init, { World } from "snake_game";
 
 init().then(_ => {
   const world = World.new();
+  const canvas = document.getElementById("snake-canvas")
+  const ctx = canvas.getContext("2s");
+  
   console.log(world, 'world')
 })
